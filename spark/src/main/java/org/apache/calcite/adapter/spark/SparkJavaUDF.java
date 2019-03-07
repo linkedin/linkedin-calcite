b15a32d1bbec7e88e0cdf2645d513bf3fe89a6e5
Min Shen <mshen@linkedin.com>
2019-03-07 14:55:52 -0800

    Add Calcite UDF that captures Spark lambda function and Spark Java UDFs.

---
+++ spark/src/main/java/org/apache/calcite/adapter/spark/SparkJavaUDF.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.calcite.adapter.spark;
+
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.schema.Function;
+import org.apache.calcite.sql.SqlFunctionCategory;
+import org.apache.calcite.sql.SqlIdentifier;
+import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.calcite.sql.type.OperandTypes;
+import org.apache.calcite.sql.type.SqlOperandTypeChecker;
+import org.apache.calcite.sql.type.SqlOperandTypeInference;
+import org.apache.calcite.sql.type.SqlReturnTypeInference;
+import org.apache.calcite.sql.validate.SqlUserDefinedFunction;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+/**
+ * Custom UDF representing a Spark Java UDF.
+ */
+public class SparkJavaUDF extends SqlUserDefinedFunction {
+  private String className;
+  private String methodName;
+  private List<String> inputClassNames;
+  private String outputClassName;
+
+  private SparkJavaUDF(SqlIdentifier opName,
+      SqlReturnTypeInference returnTypeInference,
+      SqlOperandTypeInference operandTypeInference,
+      SqlOperandTypeChecker operandTypeChecker,
+      List<RelDataType> paramTypes,
+      Function function,
+      String className,
+      String methodName,
+      List<String> inputClassNames,
+      String outputClassName) {
+    super(opName, returnTypeInference, operandTypeInference, operandTypeChecker,
+        paramTypes, function, SqlFunctionCategory.USER_DEFINED_CONSTRUCTOR);
+    this.className = className;
+    this.methodName = methodName;
+    this.inputClassNames = inputClassNames;
+    this.outputClassName = outputClassName;
+  }
+
+  public SparkJavaUDF(String name,
+      SqlReturnTypeInference returnTypeInference,
+      List<RelDataType> paramTypes,
+      Function function,
+      String className,
+      String methodName,
+      List<String> inputClassNames,
+      String outputClassName) {
+    this(new SqlIdentifier(ImmutableList.of(name), SqlParserPos.ZERO),
+        returnTypeInference,
+        null,
+        OperandTypes.ANY,
+        paramTypes,
+        function,
+        className,
+        methodName,
+        inputClassNames,
+        outputClassName);
+  }
+
+  public String getClassName() {
+    return className;
+  }
+
+  public String getMethodName() {
+    return methodName;
+  }
+
+  public List<String> getInputClassNames() {
+    return inputClassNames;
+  }
+
+  public String getOutputClassName() {
+    return outputClassName;
+  }
+}
+
+// End SparkJavaUDF.java
---
+++ spark/src/main/java/org/apache/calcite/adapter/spark/SparkObjectToColumnFunction.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.calcite.adapter.spark;
+
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.schema.Function;
+import org.apache.calcite.sql.SqlFunctionCategory;
+import org.apache.calcite.sql.SqlIdentifier;
+import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.calcite.sql.type.OperandTypes;
+import org.apache.calcite.sql.type.SqlOperandTypeChecker;
+import org.apache.calcite.sql.type.SqlOperandTypeInference;
+import org.apache.calcite.sql.type.SqlReturnTypeInference;
+import org.apache.calcite.sql.validate.SqlUserDefinedFunction;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+/**
+ * Custom UDF representing a Spark Dataset lambda function.
+ */
+public class SparkObjectToColumnFunction extends SqlUserDefinedFunction {
+
+  private SparkObjectToColumnFunction(SqlIdentifier opName,
+      SqlReturnTypeInference returnTypeInference,
+      SqlOperandTypeInference operandTypeInference,
+      SqlOperandTypeChecker operandTypeChecker,
+      List<RelDataType> paramTypes, Function function) {
+    super(opName, returnTypeInference, operandTypeInference, operandTypeChecker,
+        paramTypes, function, SqlFunctionCategory.USER_DEFINED_CONSTRUCTOR);
+  }
+
+  public SparkObjectToColumnFunction(String name, SqlReturnTypeInference returnTypeInference,
+      List<RelDataType> paramTypes, Function function) {
+    this(new SqlIdentifier(ImmutableList.of(name), SqlParserPos.ZERO), returnTypeInference,
+        null, OperandTypes.ANY, paramTypes, function);
+  }
+}
+
+// End SparkObjectToColumnFunction.java
